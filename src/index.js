6adfa013a45fdb9eb573436478115c95dd8617e5
Haroen Viaene <fingebimus@me.com>
2018-08-08 14:37:42 +0200

    normaler prettier

--- src/index.js
+++ src/index.js
@@ -9,14 +9,7 @@ const url =
 const Error = ({ failed, error, data }) =>
   failed ? (
     <div className="error">
-      Error:{' '}
-      <code>
-        {error
-          ? error.message
-          : data
-            ? data.message
-            : 'unknown'}
-      </code>
+      Error: <code>{error ? error.message : data ? data.message : 'unknown'}</code>
     </div>
   ) : null;
 
@@ -28,8 +21,7 @@ const images = {
   'instantsearch.js': 'instantsearch.js.svg',
   'javascript-client': undefined,
   'javascript-helper': undefined,
-  'react-instantsearch-native':
-    'react-instantsearch.svg',
+  'react-instantsearch-native': 'react-instantsearch.svg',
   'react-instantsearch': 'react-instantsearch.svg',
   'vue-instantsearch': 'vue-instantsearch.svg',
   fallback: 'algolia.svg',
@@ -38,10 +30,7 @@ const Sandbox = ({ name, url, id, native, repo }) => (
   <div className={`sandbox ${native ? 'native' : ''}`}>
     <a href={native ? repo : url} target="_blank">
       {name}
-      <img
-        src={images[id] || images.fallback}
-        alt="logo"
-      />
+      <img src={images[id] || images.fallback} alt="logo" />
     </a>
   </div>
 );
@@ -53,45 +42,28 @@ const nativeLibraries = [
 ];
 
 const dataToSandboxes = data =>
-  data
-    .filter(({ type }) => type === 'dir')
-    .map(({ html_url, name }) => ({
-      id: name,
-      name: name
-        .replace(/-/g, ' ')
-        .replace('instantsearch', 'InstantSearch')
-        .replace('javascript', 'JavaScript')
-        .split(' ')
-        .map(
-          s =>
-            s.slice(0, 1).toLocaleUpperCase() +
-            s.slice(1)
-        )
-        .join(' ')
-        .replace('Ios', 'iOS'),
-      url: html_url.replace(
-        'github.com',
-        'codesandbox.io/s/github'
-      ),
-      native: nativeLibraries.includes(name),
-      repo: html_url,
-    }));
+  data.filter(({ type }) => type === 'dir').map(({ html_url, name }) => ({
+    id: name,
+    name: name
+      .replace(/-/g, ' ')
+      .replace('instantsearch', 'InstantSearch')
+      .replace('javascript', 'JavaScript')
+      .split(' ')
+      .map(s => s.slice(0, 1).toLocaleUpperCase() + s.slice(1))
+      .join(' ')
+      .replace('Ios', 'iOS'),
+    url: html_url.replace('github.com', 'codesandbox.io/s/github'),
+    native: nativeLibraries.includes(name),
+    repo: html_url,
+  }));
 
 const Listing = ({ data }) => (
   <ul className="listing">
-    {dataToSandboxes(data).map(
-      ({ name, url, id, native, repo }) => (
-        <li key={name} className="listing-item">
-          <Sandbox
-            name={name}
-            url={url}
-            id={id}
-            native={native}
-            repo={repo}
-          />
-        </li>
-      )
-    )}
+    {dataToSandboxes(data).map(({ name, url, id, native, repo }) => (
+      <li key={name} className="listing-item">
+        <Sandbox name={name} url={url} id={id} native={native} repo={repo} />
+      </li>
+    ))}
   </ul>
 );
 
@@ -104,20 +76,13 @@ const App = () => (
       <p className="header-subtitle">templates</p>
     </header>
     <main>
-      <p>
-        All the Create InstantSearch App templates
-        available on CodeSandbox here:
-      </p>
+      <p>All the Create InstantSearch App templates available on CodeSandbox here:</p>
       <Fetch url={url}>
         {({ data, error, failed }) =>
           !failed && data ? (
             <Listing data={data} />
           ) : (
-            <Error
-              error={error}
-              data={data}
-              failed={failed}
-            />
+            <Error error={error} data={data} failed={failed} />
           )
         }
       </Fetch>
